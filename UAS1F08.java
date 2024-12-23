febb43f0e12f30fae26d0bd19bfe29b11dc03025
deaaww <deamarsell12@gmail.com>
2024-12-23 11:14:53 +0700

    perbaikan uas

--- UAS1F08.java
+++ UAS1F08.java
@@ -29,11 +29,23 @@ public class UAS1F08 {
                     System.out.print("\nMasukkan Nama Tim " + (i + 1) + " : ");
                     namaTim08[i] = sc.nextLine();
 
-                    System.out.print("Masukkan Skor untuk Level 1 : ");
-                    skor08[i][0] = sc.nextInt();
+                    for (int level = 0; level < 2; level++) {
+                        while (true) {
+                            System.out.print("Masukkan Skor untuk Level " + (level + 1) + " : ");
+                            int skor008 = sc.nextInt();
 
-                    System.out.print("Masukkan Skor untuk Level 2 : ");
-                    skor08[i][1] = sc.nextInt();
+                            if (skor008 < 0) {
+                                System.out.println("Skor tidak boleh negatif! Silakan masukkan ulang.");
+                            } else {
+                                if (level == 0 && skor008 < 35) {
+                                    skor008 = 0;
+                                    System.out.println("Skor Level 1 di bawah 35 dianggap 0.");
+                                }
+                                skor08[i][level] = skor008;
+                                break;
+                            }
+                        }
+                    }
                     sc.nextLine();
                 }
             } else if (pilihMenu08 == 2) {
@@ -41,18 +53,34 @@ public class UAS1F08 {
                 System.out.println("Nama Tim        Level 1    Level 2    Total");
                 for (int i = 0; i < namaTim08.length; i++) {
                     int total08 = skor08[i][0] + skor08[i][1];
-                    System.out.printf("%-15s %-10d %-10d %-10d%n", namaTim08[i], skor08[i][0], skor08[i][1], total08);
+                    if (total08 % 2 == 0) {
+                        total08 -= 8;
+                        System.out.printf("%-15s %-10d %-10d %-10d (-8)%n", namaTim08[i], skor08[i][0], skor08[i][1], total08);
+                    } else {
+                        System.out.printf("%-15s %-10d %-10d %-10d%n", namaTim08[i], skor08[i][0], skor08[i][1], total08);
+                    }
                 }
             } else if (pilihMenu08 == 3) {
                 int maksSkor08 = 0;
                 String pemenang08 = "";
+
                 for (int i = 0; i < namaTim08.length; i++) {
                     int total08 = skor08[i][0] + skor08[i][1];
+
+                    if (skor08[i][0] > 50 && skor08[i][1] > 50) {
+                        total08 += 8;
+                    }
+
+                    if (total08 % 2 == 0) {
+                        total08 -= 8;
+                    }
+
                     if (total08 > maksSkor08) {
                         maksSkor08 = total08;
                         pemenang08 = namaTim08[i];
                     }
                 }
+
                 System.out.println("Selamat Kepada Tim " + pemenang08 + " yang telah memenangkan kompetisi dengan total skor " + maksSkor08);
             } else if (pilihMenu08 == 4) {
                 System.out.println("Terima kasih program selesai");
